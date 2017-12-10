77c627669634e80f9e7bd4fa8c7488115df8eb99
wufan <997326661@qq.com>
2017-12-10 10:50:42 +0800

    test

--- chapter1/eureka-server/src/main/java/com/forezp/EurekaServerApplication.java
+++ chapter1/eureka-server/src/main/java/com/forezp/EurekaServerApplication.java
@@ -1,14 +1,25 @@
 package com.forezp;
 
+import com.serotonin.modbus4j.ip.IpParameters;
+import com.serotonin.modbus4j.ip.tcp.TcpMaster;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @EnableEurekaServer
 @SpringBootApplication
 public class EurekaServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+
+	public void get() {
+		IpParameters parameters =new IpParameters();
+		parameters.setPort(502);
+		parameters.setHost("10.0.3.243");
+		TcpMaster tcpMaster = new TcpMaster(parameters,true);
 	}
 }
